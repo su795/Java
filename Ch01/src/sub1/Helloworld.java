66bf8140044e32b85a01fe961d863232ed7dcf19
ligr00vefe <ligr00vefe@naver.com>
2020-10-27 23:35:28 +0900

    2020/04/29 Java 수업 HelloWorld 출력

---
+++ Ch01/src/sub1/Helloworld.java
@@ -0,0 +1,16 @@
+package sub1;
+
+/*
+ * 날짜 : 2020/04/29
+ * 이름 : 권기민
+ * 내용 : 자바 HelloWorld 출력하기
+ */
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello World~~~!!!");
+		System.out.println("Hello Java~~~!!!");
+	}
+}
